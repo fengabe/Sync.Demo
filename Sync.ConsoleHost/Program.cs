05782fd9b4f4d2c7e62842431a8a44187477476a
Cuong Le <lncuong@gmail.com>
2012-06-02 15:16:16 +0700

    remove rem code

--- Sync.ConsoleHost/Program.cs
+++ Sync.ConsoleHost/Program.cs
@@ -6,16 +6,6 @@ namespace Sync.ConsoleHost {
     class Program {
         static void Main(string[] args) {
             var coordinator = new RestoreCoordinator();
-
-            //var templateIds = new List<Guid>
-            //                  {
-            //                      new Guid("DFEC607E-D6AD-4BAE-A335-00F26F6D57B8"),
-            //                      new Guid("B2A5F5EA-015A-46D7-BE0F-03B5C305314E")
-            //                  };
-
-            //DateTime time = DateTime.Now;
-            //coordinator.RestoreTemplates(templateIds);
-
             var masterIds = new List<Guid>
                               {
                                   new Guid("27DF0B0A-565A-4BAE-B931-5CDD7CC04A49"),
